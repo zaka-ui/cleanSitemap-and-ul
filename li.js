1eeeb1b606a2faa697f2c3a37aafc2bdfe860f7b
zaka-ui <leassafrizakaria@gmail.com>
2023-04-13 02:15:30 +0000

    test ul list

--- li.js
+++ li.js
@@ -10,24 +10,27 @@ let html = "";
 textErea.addEventListener("input", (e) => {
   html += e.target.value;
 });
+
 submitBtn.addEventListener("click", () => {
+  textErea.value = "";
   const url = extractUrls(html);
   const links = url.toString();
-  console.log(links);
   const result = links.replaceAll(",", "\n");
-  console.log(result);
   copyValue.value = result;
-  textErea.value = "";
+  html = "";
 });
+
 function extractUrls(html) {
-  copyValue.value = "";
   if (!html.includes("ul") || !html.includes("li")) {
     showLinks.innerHTML = `<h3 class="error">Please Enter Valid Ul list</h3>`;
+    setTimeout(() => {
+      showLinks.innerHTML = `<h3 class="error"></h3>`;
+    }, 5000);
   } else {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, "text/html");
     const links = doc.querySelectorAll("a[href]");
-    const urls = [];
+    let urls = [];
     links.forEach((link) => {
       const url = link.getAttribute("href");
       urls.push(url);
@@ -37,8 +40,8 @@ function extractUrls(html) {
 }
 
 cleanIcon.addEventListener("click", () => {
-  textErea.value = "";
   copyValue.value = "";
+  html = "";
 });
 copyIcon.addEventListener("click", () => {
   copyValue.select();
